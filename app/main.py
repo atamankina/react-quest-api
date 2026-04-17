0c4f244178976ef8c99660f68381c626e12e504a
Galina Atamankina <g.atamankina@gmail.com>
2026-04-17 10:27:05 +0200

    Konfiguriert CORS.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.config import FRONTEND_ORIGIN
 from app.routers import lessons, quiz
 
 
 app = FastAPI(title="React Quest API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_ORIGIN],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(lessons.router)
 app.include_router(quiz.router)
